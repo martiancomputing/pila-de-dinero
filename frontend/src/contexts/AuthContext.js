f7149e300fbbf548300e644a237598aaf9af973c
Najerarodriguez <36144835+Najerarodriguez@users.noreply.github.com>
2021-05-30 20:19:47 -0600

    instalar react router dom y ver estado signup

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+import { auth } from '../firebase_config'
 
-export default function AuthContext() {
+const AuthContext = React.createContext()
+
+export function useAuth() {
+    return useContext(AuthContext)
+}
+
+export function AuthProvider({ children }) {
+    const [currentUser, setCurrentUser] = useState()
+    const [loading, setLoading] = useState(true)
+
+    function signup(email, password) {
+        return auth.createUserWithEmailAndPassword(email, password)
+    }
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged( user => {
+            setLoading(false)
+            setCurrentUser(user)
+        })
+
+        return unsubscribe
+    }, [])
+    
+    const value = {
+        currentUser,
+        signup
+    }
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={value}>
+            {!loading && children}
+        </AuthContext.Provider>            
     )
 }
